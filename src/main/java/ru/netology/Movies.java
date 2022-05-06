5dcb6a0daeac1edf176880946a332a943cdc149f
Макс Кузнецов <orenburg20172017@gmail.com>
2022-05-07 01:18:35 +0300

    перенес массив в тестовый класс

--- src/main/java/ru/netology/Movies.java
+++ src/main/java/ru/netology/Movies.java
@@ -2,52 +2,52 @@ package ru.netology;
 
 
 public class Movies {
-    private String[] movies = {
-            "first",
-            "second",
-            "third",
-            "forth",
-            "fifth",
-            "sixth",
-            "seventh",
-            "eighth",
-            "ninth",
-            "tenth",
-            "eleventh"
-    };
+    private String[] movies;
     private int lastMovies;
+    private  String movie;
+
+
+    public Movies(String[] movies) {
+        this.movies = movies;
+    }
 
     public Movies() {
     }
+
     public Movies(int lastMovies) {
 
         this.lastMovies = 10;
     }
 
-    public String[] addMovie(String newMovie) {
 
-        String[] tmp = new String[movies.length + 1];
+    public String[] addMovie(String[] movies) {
+
+        String newMovie = new String("twelve");
+        int length = movies.length + 1;
+        String[] tmp = new String[length];
         for (int i = 0; i < movies.length; i++) {
             tmp[i] = movies[i];
         }
         tmp[tmp.length - 1] = newMovie;
 
         movies = tmp;
-        return tmp;
+        return movies;
     }
-    public String[] findLast() {
-        int j = movies.length - lastMovies;
 
-        String[] tmp = new String[movies.length - j];
+    public String[] findLast(String[] movies) {
+
+        int fistMovies = movies.length - lastMovies;
+
+        String[] tmp = new String[movies.length - fistMovies];
         int index = 0;
-        for (int i = movies.length - 1; i > j - 1;i = i -1) {
+        for (int i = movies.length - 1; i > fistMovies - 1; i = i - 1) {
             tmp[index] = movies[i];
             index++;
         }
         return tmp;
     }
 
-    public String[] findAll() {
+    public String[] findAll(String[] movies) {
         return movies;
     }
 }
--- src/test/java/ru/netology/MoviesTest.java
+++ src/test/java/ru/netology/MoviesTest.java
@@ -5,24 +5,63 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class MoviesTest {
+    String[] movies = new String[]{
+            "first",
+            "second",
+            "third",
+            "forth",
+            "fifth",
+            "sixth",
+            "seventh",
+            "eighth",
+            "ninth",
+            "tenth",
+            "eleventh"
+    };
+
 
     @Test
     public void findAll() {
         Movies mov = new Movies();
-        mov.findAll();
+
+        mov.findAll(movies);
 
         String[] expected = {"first", "second", "third", "forth", "fifth", "sixth", "seventh", "eighth", "ninth", "tenth", "eleventh"};
-        String[] actual = mov.findAll();
+        String[] actual = mov.findAll(movies);
 
         assertArrayEquals(expected, actual);
     }
-    @Test
-    public void findLast() {
-        Movies mov = new Movies(10);
-        mov.findLast();
 
-        String[] expected = {"eleventh", "tenth", "ninth", "eighth", "seventh", "sixth", "fifth", "forth", "third","second"};
-        String[] actual = mov.findLast();
+    @Test
+    public void findLastIfMovieBigger() {
+        Movies mov = new Movies(10);
+
+        mov.findLast(movies);
+
+        String[] expected = {"eleventh", "tenth", "ninth", "eighth", "seventh", "sixth", "fifth", "forth", "third", "second"};
+        String[] actual = mov.findLast(movies);
+
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void findLastIfMovieLess() {
+        Movies mov = new Movies(13);
+        mov.findLast(movies);
+
+        String[] expected = {"eleventh", "tenth", "ninth", "eighth", "seventh", "sixth", "fifth", "forth", "third", "second"};
+        String[] actual = mov.findLast(movies);
+
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void findLastIfMovieEqual() {
+        Movies mov = new Movies(11);
+        mov.findLast(movies);
+
+        String[] expected = {"eleventh", "tenth", "ninth", "eighth", "seventh", "sixth", "fifth", "forth", "third", "second"};
+        String[] actual = mov.findLast(movies);
 
         assertArrayEquals(expected, actual);
     }
@@ -32,8 +71,8 @@ class MoviesTest {
         Movies mov = new Movies();
 
         String[] expected = {"first", "second", "third", "forth", "fifth", "sixth", "seventh", "eighth", "ninth", "tenth", "eleventh", "twelve"};
-        String[] actual = mov.addMovie("twelve");
+        String[] actual = mov.addMovie(movies);
 
-        assertArrayEquals( expected, actual);
+        assertArrayEquals(expected, actual);
     }
 }
